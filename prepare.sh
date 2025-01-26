ac6d2d08b9b1ffd2e307712f2e5899517121d4c7
GiuseppeLorenzoDiPrima <giuseppelorenzo.diprima@unikorestudent.it>
2025-01-26 16:06:41 +0100

    Commit finale

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,4 @@
 pip install -r requirements.txt
 
 # Copy the dataset from a github repository
-git clone https://github.com/GiuseppeLorenzoDiPrima/dataset_classificatore_impronte_digitali.git
+git clone https://github.com/GiuseppeLorenzoDiPrima/dataset.git
